7a6d9c4e12b46768d00909e2814b41c284df2cfa
safdar211ali <safdar211@hotmail.com>
2024-07-27 09:56:01 +0500

    udapated

--- interiofloors_reports/interiofloors_reports/report/customer_balances_custom/customer_balances_custom.js
+++ interiofloors_reports/interiofloors_reports/report/customer_balances_custom/customer_balances_custom.js
@@ -15,61 +15,61 @@ frappe.query_reports["Customer Balances Custom"] = {
 			"fieldtype": "Date",
 			"default": frappe.datetime.get_today()
 		},
-		// {
-		// 	"fieldname":"ageing_based_on",
-		// 	"label": __("Ageing Based On"),
-		// 	"fieldtype": "Select",
-		// 	"options": 'Posting Date\nDue Date',
-		// 	"default": "Due Date"
-		// },
-		// {
-		// 	"fieldname":"range1",
-		// 	"label": __("Ageing Range 1"),
-		// 	"fieldtype": "Int",
-		// 	"default": "30",
-		// 	"reqd": 1
-		// },
-		// {
-		// 	"fieldname":"range2",
-		// 	"label": __("Ageing Range 2"),
-		// 	"fieldtype": "Int",
-		// 	"default": "60",
-		// 	"reqd": 1
-		// },
-		// {
-		// 	"fieldname":"range3",
-		// 	"label": __("Ageing Range 3"),
-		// 	"fieldtype": "Int",
-		// 	"default": "90",
-		// 	"reqd": 1
-		// },
-		// {
-		// 	"fieldname":"range4",
-		// 	"label": __("Ageing Range 4"),
-		// 	"fieldtype": "Int",
-		// 	"default": "120",
-		// 	"reqd": 1
-		// },
-		// {
-		// 	"fieldname":"finance_book",
-		// 	"label": __("Finance Book"),
-		// 	"fieldtype": "Link",
-		// 	"options": "Finance Book"
-		// },
-		// {
-		// 	"fieldname":"cost_center",
-		// 	"label": __("Cost Center"),
-		// 	"fieldtype": "Link",
-		// 	"options": "Cost Center",
-		// 	get_query: () => {
-		// 		var company = frappe.query_report.get_filter_value('company');
-		// 		return {
-		// 			filters: {
-		// 				'company': company
-		// 			}
-		// 		}
-		// 	}
-		// },
+		{
+			"fieldname":"ageing_based_on",
+			"label": __("Ageing Based On"),
+			"fieldtype": "Select",
+			"options": 'Posting Date\nDue Date',
+			"default": "Due Date"
+		},
+		{
+			"fieldname":"range1",
+			"label": __("Ageing Range 1"),
+			"fieldtype": "Int",
+			"default": "30",
+			"reqd": 1
+		},
+		{
+			"fieldname":"range2",
+			"label": __("Ageing Range 2"),
+			"fieldtype": "Int",
+			"default": "60",
+			"reqd": 1
+		},
+		{
+			"fieldname":"range3",
+			"label": __("Ageing Range 3"),
+			"fieldtype": "Int",
+			"default": "90",
+			"reqd": 1
+		},
+		{
+			"fieldname":"range4",
+			"label": __("Ageing Range 4"),
+			"fieldtype": "Int",
+			"default": "120",
+			"reqd": 1
+		},
+		{
+			"fieldname":"finance_book",
+			"label": __("Finance Book"),
+			"fieldtype": "Link",
+			"options": "Finance Book"
+		},
+		{
+			"fieldname":"cost_center",
+			"label": __("Cost Center"),
+			"fieldtype": "Link",
+			"options": "Cost Center",
+			get_query: () => {
+				var company = frappe.query_report.get_filter_value('company');
+				return {
+					filters: {
+						'company': company
+					}
+				}
+			}
+		},
 		{
 			"fieldname":"party_type",
 			"label": __("Party Type"),
@@ -123,16 +123,16 @@ frappe.query_reports["Customer Balances Custom"] = {
 			"fieldtype": "Link",
 			"options": "Sales Person"
 		},
-		// {
-		// 	"fieldname":"based_on_payment_terms",
-		// 	"label": __("Based On Payment Terms"),
-		// 	"fieldtype": "Check",
-		// },
-		// {
-		// 	"fieldname":"show_future_payments",
-		// 	"label": __("Show Future Payments"),
-		// 	"fieldtype": "Check",
-		// },
+		{
+			"fieldname":"based_on_payment_terms",
+			"label": __("Based On Payment Terms"),
+			"fieldtype": "Check",
+		},
+		{
+			"fieldname":"show_future_payments",
+			"label": __("Show Future Payments"),
+			"fieldtype": "Check",
+		},
 		{
 			"fieldname":"show_gl_balance",
 			"label": __("Show GL Balance"),
