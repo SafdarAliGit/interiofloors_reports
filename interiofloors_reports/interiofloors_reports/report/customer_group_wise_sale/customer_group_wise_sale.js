727fd9b0990e27cb84b264451f546b28bd6b68e9
safdar211ali <safdar211@hotmail.com>
2024-12-13 21:55:09 +0500

    updated

--- interiofloors_reports/interiofloors_reports/report/customer_group_wise_sale/customer_group_wise_sale.js
+++ interiofloors_reports/interiofloors_reports/report/customer_group_wise_sale/customer_group_wise_sale.js
@@ -1,6 +1,7 @@
 // Copyright (c) 2024, portal and contributors
 // For license information, please see license.txt
 
+
 frappe.query_reports["Customer Group-wise Sale"] = {
     "filters": [
         {
